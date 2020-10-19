7640fc55e9cf1115f5c27c7aca17d87b8d601701
Cristi Gutierrez <cristigtzname@gmail.com>
2020-10-19 16:16:44 -0400

    commit

--- app.js
+++ app.js
@@ -1,39 +1,21 @@
-window.addEventListener('load', () => {
-    const sounds = document.querySelectorAll(".sound");
-    const pads = document.querySelectorAll(".pads div");
-    const visual = document.querySelector('.visual');
-    const colors = [
-        "#60d394",
-        "#d36060",
-        "#c060d3",
-        "#d3d160",
-        "#bd3535",
-        "#35bdab"
-    ];
+//Time Top
+
+var liveTime = new Time();
+document.getElementById('time').innerHTML = liveTime.getTime();
 
 
+var currentPlayer;
 
+function EvalSound(soundobj) {
 
-    //Lets get going with the sound here
-    pads.forEach((pad, index) => {
-        pad.addEventListener('click', function() {
-            sounds[index].currentTime = 0;
-            sounds[index].play();
-
-            createBubbles(index);
-        });
-    });
-
-    //Create function that makes bubbles 
-    const createBubbles = (index) => {
-        const bubble = document.createElement("div");
-        visual.appendChild(bubble);
-        bubble.style.backgroundColor = colors[index];
-        bubble.style.animation = 'jump 1s ease';
-        bubble.addEventListener('animationend', function() {
-            visual.removeChild(this);
-        })
+    var thissound = document.getElementById(soundobj);
+    if (currentPlayer && currentPlayer != thissound) {
+        currentPlayer.pause();
     }
-
-
-});
+    if (thissound.paused)
+        thissound.play();
+    else
+        thissound.pause();
+    thissound.currentTime = 0;
+    currentPlayer = thissound;
+}
