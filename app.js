e8cc979d0036b9527258732e3946417ddeb466d7
Cristi Gutierrez <cristigtzname@gmail.com>
2020-10-25 20:51:47 -0400

    commit

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 let playSong = document.getElementById("play");
 let previousSong = document.getElementById("prev");
 let nextSong = document.getElementById("next");
+
+
+function playSong() {
+    let audio = document.querySelector("track");
+    audio.play();
+}
