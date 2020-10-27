069589095f9377782b8691f0bb616a23ebf5af43
Cristi Gutierrez <cristigtzname@gmail.com>
2020-10-27 13:36:10 -0400

    commit

--- app.js
+++ app.js
@@ -2,16 +2,7 @@ let playSong = document.getElementById("play");
 let previousSong = document.getElementById("prev");
 let nextSong = document.getElementById("next");
 
-
-function playSong() {
-    document.getElementsByClassName('pad').;
-
-
-    function myFunction() {
-        document.getElementById("field2").value = document.getElementById("field1").value;
-    }
-}
-
-const play = () => {
-    playSong.play();
+function play() {
+    var audio = document.getElementsByTagName("audio");
+    audio.play();
 }
